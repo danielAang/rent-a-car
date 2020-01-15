56627e59ced5d71822f6834ef51fe35235721b65
daniel.sena <danielsena04@gmail.com>
2020-01-14 22:52:36 -0300

    Adicionando rotas para acessar api externa fipe

--- src/routes/carrosRoutes.js
+++ src/routes/carrosRoutes.js
@@ -1,11 +1,60 @@
 const express = require('express');
+const FipeService = require('../services/fipeService');
+const logger = require('../config/winston');
 
 const router = express.Router();
+const fipeService = new FipeService();
 
 module.exports = () => {
-  router.get('/', (req, res) => {
-    res.json({ message: 'carros' });
-  });
+	router.get('/marcas', (req, res) => {
+		fipeService
+			.getMarcas(FipeService.tipoVeiculo.CARRO)
+			.then((response) => {
+				res.json(response.data);
+			})
+			.catch((err) => {
+				logger.error({ message: err });
+				res.status(500).json({ message: 'Falha ao acessar marcas' });
+			});
+	});
 
-  return router;
+	router.get('/marcas/:marca/modelos', (req, res) => {
+		const { marca } = req.params;
+		fipeService
+			.getModelos(FipeService.tipoVeiculo.CARRO, marca)
+			.then((response) => {
+				res.json(response.data.modelos);
+			})
+			.catch((err) => {
+				logger.error({ message: err });
+				res.status(500).json({ message: 'Falha ao acessar modelos' });
+			});
+	});
+
+	router.get('/marcas/:marca/modelos/:modelo/anos', (req, res) => {
+		const { marca, modelo } = req.params;
+		fipeService
+			.getAnos(FipeService.tipoVeiculo.CARRO, marca, modelo)
+			.then((response) => {
+				res.json(response.data);
+			})
+			.catch((err) => {
+				logger.error({ message: err });
+				res.status(500).json({ message: 'Falha ao acessar anos' });
+			});
+	});
+
+	router.get('/marcas/:marca/modelos/:modelo/anos/:ano', (req, res) => {
+		const { marca, modelo, ano } = req.params;
+		fipeService
+			.getValor(FipeService.tipoVeiculo.CARRO, marca, modelo, ano)
+			.then((response) => {
+				res.json(response.data);
+			})
+			.catch((err) => {
+				logger.error({ message: err });
+				res.status(500).json({ message: 'Falha ao acessar valor' });
+			});
+	});
+	return router;
 };
--- src/services/fipeService.js
+++ src/services/fipeService.js
@@ -1,20 +1,38 @@
 const axios = require('axios').default;
-
-const tipoVeiculo = {
-  CARRO: 'carros',
-  MOTOS: 'motos',
-  CAMINHOES: 'caminhoes',
-};
+const logger = require('../config/winston');
 
 class FipeService {
-  constructor() {
-    this.baseUrl = 'https://parallelum.com.br/fipe/api/v1';
-  }
+	static tipoVeiculo = {
+		CARRO: 'carros',
+		MOTOS: 'motos',
+		CAMINHOES: 'caminhoes',
+	};
 
-  getMarcas(tipo) {
-    const url = `${this.baseUrl}/${tipo}/marcas`;
-    return axios.get(url);
-  }
+	static baseUrl = 'https://parallelum.com.br/fipe/api/v1';
+
+	getMarcas(tipo) {
+		const url = `${FipeService.baseUrl}/${tipo}/marcas`;
+		logger.info(`Acessando url: ${url}`);
+		return axios.get(url);
+	}
+
+	getModelos(tipo, marca) {
+		const url = `${FipeService.baseUrl}/${tipo}/marcas/${marca}/modelos`;
+		logger.info(`Acessando url: ${url}`);
+		return axios.get(url);
+	}
+
+	getAnos(tipo, marca, modelo) {
+		const url = `${FipeService.baseUrl}/${tipo}/marcas/${marca}/modelos/${modelo}/anos`;
+		logger.info(`Acessando url: ${url}`);
+		return axios.get(url);
+	}
+
+	getValor(tipo, marca, modelo, ano) {
+		const url = `${FipeService.baseUrl}/${tipo}/marcas/${marca}/modelos/${modelo}/anos/${ano}`;
+		logger.info(`Acessando url: ${url}`);
+		return axios.get(url);
+	}
 }
 
 module.exports = FipeService;
